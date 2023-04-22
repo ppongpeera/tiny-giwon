dfe8d6b77ab0eeeb2c8cf5594488fcbe71cebc4b
BoomWSL <boom@live.se>
2023-04-22 13:57:54 +0700

    Init after install all node-modules

---
+++ readme.md
@@ -0,0 +1,2 @@
+react-bootstrap -> https://react-bootstrap.github.io/getting-started/introduction
+react-router-dom -> https://reactrouter.com/en/main/start/tutorial
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './components/jsx/App'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Dashboard from './components/jsx/Dashboard';
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(  
+  <Dashboard />
+)
